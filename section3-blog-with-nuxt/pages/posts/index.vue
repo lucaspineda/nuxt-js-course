f218660c040f4f124c1813eaa2d19f768cce5227
lucaspc <lucas.pineda@hotmail.com>
2020-02-06 08:34:55 -0300

    create posts page

---
+++ section3-blog-with-nuxt/pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="posts-pages">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.post-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
