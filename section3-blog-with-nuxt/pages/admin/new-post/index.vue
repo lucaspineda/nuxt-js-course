f7d5e9968c5daad4a4a8207c7e362d4af790db7d
Lucas <lucas.pineda@hotmail.com>
2020-02-10 22:18:25 -0300

    create new post pages

---
+++ section3-blog-with-nuxt/pages/admin/new-post/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="admin-new-post-page">
+      <section class="new-post-form">
+            <form @submit.prevent="onSave">
+                <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+                <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+                <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+                <AppControlInput
+                    control-type="textarea"
+                    v-model="editedPost.content">Content</AppControlInput>
+                <AppButton type="submit">Save</AppButton>
+                <AppButton
+                    type="button"
+                    style="margin-left: 10px"
+                    btn-style="cancel"
+                    @click="onCancel">Cancel
+                </AppButton>
+            </form>
+      </section>
+  </div>
+</template>
+
+<script>
+import AppControlInput from '@/components/UI/AppControlInput.vue'
+import AppButton from '@/components/UI/AppButton.vue'
+
+export default {
+    components: {
+        AppControlInput,
+        AppButton
+    },
+
+    data() {
+        return {
+            editedPost: {
+                author: '',
+                title: '',
+                thumbnailLink: '',
+                content: ''
+            }
+        }
+    },
+    methods: {
+        onSave() {
+            console.log(this.editedPost)
+        },
+        onCancel() {
+            this.$router.push('/admin')
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
