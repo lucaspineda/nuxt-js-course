fe88d20da4ef0113eec40522d2f662fba0ad8fdf
Lucas <lucas.pineda@hotmail.com>
2020-02-26 08:43:57 -0300

    using async data in a unique post

--- section3-blog-with-nuxt/pages/posts/_id/index.vue
+++ section3-blog-with-nuxt/pages/posts/_id/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="single-post-page">
       <section class="post">
-          <h1 class="post-title">Title of the post</h1>
+          <h1 class="post-title"> {{ loadedPost.title}}</h1>
           <div class="post-details">
-              <div class="post-detail">Last updated on XXXXX</div>
-              <div class="post-detail">Writen by lucas</div>
+              <div class="post-detail">Last updated on {{ loadedPost.updateDate}}</div>
+              <div class="post-detail">Writen by {{ loadedPost.author}}</div>
           </div>
-          <p class="post-content">content of the post</p>
+          <p class="post-content">{{ loadedPost.content}}</p>
       </section>
       <section class="post-feedback">
           <p>let me know what you think of the post, send an email to <a href="mailto:lucas.pineda@hotmail.com">lucas.pineda@hotmail.com</a>.</p>
@@ -16,6 +16,22 @@
 
 <script>
 export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: '1',
+          title: 'First Post (ID: ' + context.route.params.id +')',
+          previewText: 'This is our first post!',
+          author: 'lucas pineda',
+          updateDate: new Date(),
+          content: 'this is any content',
+          thumbnail: 'https://www.ubuntupit.com/wp-content/uploads/2017/11/Best-Linux-Code-Editor-Top-10-Reviewed-and-Compared.jpeg'
+        }
+      })
+    },1000)
+    
+  },
 
 }
 </script>
